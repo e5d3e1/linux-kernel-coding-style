d29e07bd0e12d52ee58727d3c4883f8e5ea76dfd
vitalii.ostapets <e5d3e1@gmail.com>
2023-09-28 15:46:09 +0300

    fix

--- src/main/scala/org/style/coding/LinuxKernelCodingStylePlugin.scala
+++ src/main/scala/org/style/coding/LinuxKernelCodingStylePlugin.scala
@@ -2,28 +2,31 @@ package org.style.coding
 
 class LinuxKernelCodingStylePlugin {
 
-  def transform(text: String, limit: Int): String = if (text.length <= limit) {
+  def transform(text: String, limit: Int): String = if (text.length <= limit)
     text
-  } else {
-    text.split("\\s+").foldLeft(Seq[String]())((result, word) => result match {
-        case Nil =>
-          if (word.length > limit) {
-            println(s"Warning word $word is longest $limit")
-          }
-          Seq(word)
-        case x :: xs =>
-          val maybeNextLine = s"$x $word"
-          if (maybeNextLine.length <= limit) {
-            maybeNextLine +: xs
-          } else {
-            if (word.length > limit) {
-              println(s"Warning word $word is longest $limit")
-            }
-            word +: x +: xs
-          }
-      })
-      .reverse
-      .mkString("", "<br>\n", "<br>")
+  else
+    transform(text.split("\\s+"), limit)
+
+  private def transform(words: Array[String], limit: Int): String = words
+    .foldLeft(Seq[String]())((result, word) => result match {
+      case Nil =>
+        checkMaxWordSize(limit, word)
+        Seq(word)
+      case xs :+ x =>
+        val maybeNextLine = s"$x $word"
+        if (maybeNextLine.length <= limit) {
+          xs :+ maybeNextLine
+        } else {
+          checkMaxWordSize(limit, word)
+          xs :+ x :+ word
+        }
+    })
+    .mkString("", "<br>\n", "<br>")
+
+  private def checkMaxWordSize(limit: Int, word: String): Unit = {
+    if (word.length > limit) {
+      println(s"Warning word $word is longest $limit")
+    }
   }
 }
 
